9d7043ed01bc459357e0eee874874e5c37d5f125
Sumit5089 <sumitgaikwad930@gmail.com>
2024-07-10 20:49:52 +0530

    .env setup

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname,"public")));
 const store = MongoStore.create({
   mongoUrl: DB_URL,
   crypto: {
-    secret: "mysupersecretcode"
+    secret: process.env.SECRET,
   },
   touchAfter: 24 * 3600,
 });
@@ -62,7 +62,7 @@ store.on("error", () => {
 
 const sessionOptions = {
   store,
-  secret: "mysupersecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie:{
